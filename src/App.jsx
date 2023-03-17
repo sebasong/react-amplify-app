b454dc4fcf544d02059003eb5f1c0e69f6dbdf7b
Your Namesebastian <you@example.comsebasong2000@gmail.com>
2023-03-17 15:51:57 +1100

    the deployment

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,7 @@ function Contacts() {
   const [search, setSearch] = useState('');
   
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setContacts(data));
-  }, []);
+
 
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(search.toLowerCase())
@@ -20,7 +16,26 @@ function Contacts() {
 
   return (
     <div>
-     
+      <div className="Logo">
+        <img src={logoImg} alt="logo" className="logo" />
+        <h1>Resonate Contacts</h1>
+      </div>
+      
+      <div class="topnav">
+      <a class="active" href="#home">Home</a>
+    </div>
+    
+    <div className='searchContainer'>
+      <input
+         type="text"
+         value={search}
+          onChange={e => setSearch(e.target.value)}
+         placeholder="Search contacts"
+       />
+        <button className="add-contact-button">+ Add New Contact</button>
+    </div>
+      
+      
     </div>
   );
 }
