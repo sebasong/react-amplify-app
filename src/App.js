7ee08e7a83d9dd154febebafa137dc7014d611c5
Your Namesebastian <you@example.comsebasong2000@gmail.com>
2023-03-17 15:23:36 +1100

    the deployment

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import CollapsibleButton from './collapsible.jsx' ;
-import logoImg from "./logo.png";
 
-function Contacts() {
-  const [contacts, setContacts] = useState([]);
-  const [search, setSearch] = useState('');
-  
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setContacts(data));
-  }, []);
-
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(search.toLowerCase())
-  );
-
+function App() {
   return (
-    <div>
-      <div className="Logo">
-        <img src={logoImg} alt="logo" className="logo" />
-        <h1>Resonate Contacts</h1>
-      </div>
-      
-      <div class="topnav">
-      <a class="active" href="#home">Home</a>
-    </div>
-    
-    <div className='searchContainer'>
-      <input
-         type="text"
-         value={search}
-          onChange={e => setSearch(e.target.value)}
-         placeholder="Search contacts"
-       />
-        <button className="add-contact-button">+ Add New Contact</button>
-    </div>
-      
-      <div>
-        {filteredContacts.map(contact => (
-          <CollapsibleButton key={contact.id} title={contact.name} className = "CustomButton" contact = {contact}>
-        </CollapsibleButton>
-      ))}
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-
-
-export default Contacts;
+export default App;
